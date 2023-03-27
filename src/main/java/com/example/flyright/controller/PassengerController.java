6bf3cb9e12107b892d37e8f37c053af64a24beca
Bolaji Disu <abolajiidiisu@gmail.com>
2023-03-27 12:36:14 +0100

    added cors to passenger controller

--- src/main/java/com/example/flyright/controller/PassengerController.java
+++ src/main/java/com/example/flyright/controller/PassengerController.java
@@ -10,6 +10,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 import java.time.ZonedDateTime;
 @RestController
+@CrossOrigin("*")
 public class PassengerController {
     @Autowired
     PassengerService passengerService;
